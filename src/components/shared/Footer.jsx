0c8477f0915388c185c8f65f67eb99e9d1ca5711
MuhammadNoumanAfzal <mnoumanafzal4@gmail.com>
2026-04-14 16:19:44 +0500

    Refactor code structure for improved readability and maintainability

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
         <div>
           <Link to="/" className="inline-flex">
             <img
-              src="/home/logo.png"
+              src="/home/logo2.png"
               alt="Lunsjavtale"
               className="h-14 w-auto object-contain"
             />
