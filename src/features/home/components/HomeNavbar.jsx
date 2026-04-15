dc03d13344cd7b7a9ec4d64efce680c9802cbafd
MuhammadNoumanAfzal <mnoumanafzal4@gmail.com>
2026-04-15 20:58:37 +0500

    feat(navbar): adjust padding in HomeNavbar for improved layout

--- src/features/home/components/HomeNavbar.jsx
+++ src/features/home/components/HomeNavbar.jsx
@@ -7,7 +7,7 @@ export default function HomeNavbar() {
 
   return (
     <header className="w-full">
-      <div className="flex items-center justify-between px-4 py-2">
+      <div className="flex items-center justify-between px-10 py-2">
         <Link to="/" className="flex items-center">
           <img
             src="/home/logo.png"
